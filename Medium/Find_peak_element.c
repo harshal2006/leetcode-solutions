677a488027225c0d9ab8a301822396d0123bab63
harshal2006 <72093739+harshal2006@users.noreply.github.com>
2025-08-24 16:33:05 +0530

    Update Find_peak_element.c

--- Medium/Find_peak_element.c
+++ Medium/Find_peak_element.c
@@ -1,11 +1,23 @@
 int findPeakElement(int* nums, int numsSize) {
     int max=nums[0],i,j,peak=0;
-    for (i=0;i<numsSize;i++){
-        if (nums[i]>max){
-            max=nums[i];
-            peak=i;
+    if(numsSize==1){
+        return 0;
+
+    }
+    if (nums[0]>nums[1]){
+        return 0;
+    }
+    if (nums[numsSize-1]>nums[numsSize-2]){
+        return numsSize-1;
+    }
+    for (i=1;i<numsSize-1;i++){
+        if (nums[i]>nums[i+1]&& nums[i]>nums[i-1]){
+            return i;  
         }
     }
-    return peak;
-    
+    return -1;
+
 }
+    
+    
+    
